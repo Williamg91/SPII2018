191fa3ebcb205da2dbbb0afbde3430f89c3824a1
William <william.geismar@gmail.com>
2018-06-03 11:54:52 +0200

    Lidt eksempler og sjov til jer

---
+++ src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+
+public class Controller {
+
+   @FXML
+    private LineChart LineChart;
+
+
+   public void opdaterGraf
+}
